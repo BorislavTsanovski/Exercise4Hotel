3180da9a5225f37591c0ce35432117467bb3b2c9
Denitsa <denitsabebrevenska@gmail.com>
2025-01-18 13:40:30 +0200

    Implemented Confirm Reservation form functionality

--- solution.js
+++ solution.js
@@ -26,46 +26,19 @@ function cleanData(e) {
 }
 
 document
-  .querySelector("#search-back-btn")
-  .addEventListener("click", (e) => fillSearchForm(e));
+  .querySelector("#confirm-back-btn")
+  .addEventListener("click", (e) => getBackToPersonalData(e));
 
-function fillSearchForm(e) {
+function getBackToPersonalData(e) {
   e.preventDefault();
-  changeContent("search-form-content");
-  document.querySelector("#check-in").value = reservation.startDate;
-  document.querySelector("#check-out").value = reservation.endDate;
-  document.querySelector("#people").value = reservation.guestsCount;
-}
-
-document.querySelectorAll(".room-type").forEach((room) => {
-  room.addEventListener("click", (e) => selectRoomType(e));
-});
-
-function selectRoomType(e) {
-  let myTarget = undefined;
-  e.preventDefault;
-  if (e.target.querySelector("img") != null) {
-    myTarget = e.target;
-  } else {
-    myTarget = e.target.parentElement;
-  }
-  document
-    .querySelectorAll(".room-type")
-    .forEach((room) => room.classList.remove("selected-room"));
-  myTarget.classList.add("selected-room");
+  changeContent("guest-details-form-content");
 }
 
 document
-  .querySelector("#search-next-btn")
-  .addEventListener("click", (e) => findRoom(e));
+  .querySelector("#confirm-reservation")
+  .addEventListener("click", (e) => showThanksPage(e));
 
-function findRoom(e) {
+function showThanksPage(e) {
   e.preventDefault();
-  const roomInfo =
-    e.target.parentElement.parentElement.querySelector(
-      ".selected-room h4"
-    ).textContent;
-  reservation.roomType = roomInfo;
-  console.log(reservation);
-  changeContent("guest-details-form-content");
+  changeContent("thank-you-content");
 }
